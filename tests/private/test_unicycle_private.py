719815da3e674b666c0feb8b0d40f84266af9066
mvielmet <mvielmet@umich.edu>
2026-09-16 11:40:26 -0400

    updated version v0.1.3 again

--- tests/private/test_unicycle_private.py
+++ tests/private/test_unicycle_private.py
@@ -1,5 +1,9 @@
-from dasc_lab.dynamics import UnicycleDynamics
+import numpy as np
+
+from dasc_lab.dynamics import Unicycle2D
 
 
 def test_forward_motion_accepts_negative_speed() -> None:
-    assert UnicycleDynamics().f((3.0, -0.25), -1.5) == (-1.5, -0.25)
+    assert Unicycle2D().f(
+        np.array([3.0, -0.25]).reshape(-1, 1), np.array([-1.5]).reshape(-1, 1)
+    ) == np.array([-1.5, -0.25]).reshape(-1, 1)
